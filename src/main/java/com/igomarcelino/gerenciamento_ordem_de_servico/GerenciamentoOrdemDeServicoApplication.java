bf1124c8929ce6fb29c4719e33ddae15ae677363
Igo Marcelino <geslucian@hotmail.com>
2024-11-23 15:36:48 -0300

    Inititial commit

---
+++ src/main/java/com/igomarcelino/gerenciamento_ordem_de_servico/GerenciamentoOrdemDeServicoApplication.java
@@ -0,0 +1,13 @@
+package com.igomarcelino.gerenciamento_ordem_de_servico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoOrdemDeServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoOrdemDeServicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igomarcelino/gerenciamento_ordem_de_servico/GerenciamentoOrdemDeServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igomarcelino.gerenciamento_ordem_de_servico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoOrdemDeServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
